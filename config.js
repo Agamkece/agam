a23d33e8943af3d2e5e6574f5b8c6f46f38b23c7
Agamkece <116334274+Agamkece@users.noreply.github.com>
2022-10-21 13:37:53 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6285892696324']
+global.ownernomer = "6285892696324"
+global.premium = ['6285892696324']
+global.packname = 'AGAM BOT'
+global.author = 'MAS AGAM'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
